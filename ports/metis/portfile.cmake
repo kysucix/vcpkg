bdb18b354aa3c51a0fd23cf3c339f8a183b6243d
Silvano Galliani <sigallia@microsoft.com>
2022-07-11 11:15:28 +0200

    Update metis and use github

--- ports/metis/portfile.cmake
+++ ports/metis/portfile.cmake
@@ -1,18 +1,12 @@
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 set(OPTIONS -DSHARED=OFF)
 
-set(METIS_VERSION 5.1.0)
-
-vcpkg_download_distfile(ARCHIVE
-    URLS "http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/metis-${METIS_VERSION}.tar.gz"
-    FILENAME "metis-${METIS_VERSION}.tar.gz"
-    SHA512 deea47749d13bd06fbeaf98a53c6c0b61603ddc17a43dae81d72c8015576f6495fd83c11b0ef68d024879ed5415c14ebdbd87ce49c181bdac680573bea8bdb25
-)
-
-vcpkg_extract_source_archive_ex(
+vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    ARCHIVE ${ARCHIVE}
-    REF ${METIS_VERSION}
+    REPO KarypisLab/METIS
+    REF v5.1.1-DistDGL-v0.5
+    SHA512 18b43bdc6239f6b4a60e2d43c3e51ee4c2b829b4ebaa45df3bd9dc5e8d452fe6b1e33151faa566b6481b1ad519fed2fb84fab7962030bc68e3ab5b5767908e22
+    HEAD_REF master
     PATCHES
         enable-install.patch
         disable-programs.patch
